d7ac9c48a624981f1301afcf5acae9778050ac68
philip <pmutua@live.com>
2020-07-13 00:00:00 +0300

    feat: classify class view & methods added

--- web/app.py
+++ web/app.py
@@ -46,3 +46,74 @@ class Register(Resource):
             "msg": "You successfully signed up for this API"
         }
         return jsonify(retJson)
+
+def verifyCredentials(username, password):
+    if not UserExists(username):
+        return generateReturnDictionary(301,"Invalid Username"), True
+    correct_pw = verify_pw(username, password)
+    if not correct_pw:
+        return generateReturnDictionary(302,"Invalid Password"), True
+
+    return None, False
+
+def verify_pw(username,password):
+    if not UserExists(username):
+        return False 
+    hashed_pw = users.find({
+        "Username": username
+    })[0]["Password"]
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
+        return True
+    else:
+        False
+
+def generateReturnDictionary(status,msg):
+    retJson= {
+        "status": status,
+        "msg": msg
+    }
+    return retJson
+
+
+
+class Classify(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData['username']
+        password = postedData['password']
+        url = postedData['url']
+
+        retJson, error = verifyCredentials(username,password)
+        if error:
+            return jsonify(retJson)
+
+        tokens = users.find({
+            "Username":username,
+        })[0]["Tokens"]
+
+        #TODO
+        if tokens <=0:
+            return jsonify(generateReturnDictionary(303, "Not enough Tokens!"))
+
+        r = requests.get(url)
+        retJson = {}
+        with open("temp.jpg", "wb") as f:
+            f.write(r.content)
+            proc = subprocess.Popen('python classify_image.py --model_dir=. --image_file=./temp.jpg')
+            proc.communicate()[0]
+            proc.wait()
+
+            with open("text.txt") as g:
+                retJson = json.load(g)
+
+        users.update({
+            "Username":username
+        },{
+            "$set":{
+                "Tokens": tokens-1
+            }
+        }
+        )
+        return retJson
